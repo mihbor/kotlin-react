9db45d3188b77641c6cc05b0d0215120f9e623e5
Michal.Borowiecki <michal.borowiecki@10xbanking.com>
2021-01-29 11:20:50 +0000

    more space pioneers

--- src/commonMain/kotlin/PersonData.kt
+++ src/commonMain/kotlin/PersonData.kt
@@ -3,7 +3,12 @@ val spacePioneers = listOf(
   Person("Tycho Brahe", 1546, 1601, "https://upload.wikimedia.org/wikipedia/commons/2/2b/Tycho_Brahe.JPG", "https://en.wikipedia.org/wiki/Tycho_Brahe"),
   Person("Johannes Kepler", 1571, 1630, "https://upload.wikimedia.org/wikipedia/commons/d/d4/Johannes_Kepler_1610.jpg", "https://en.wikipedia.org/wiki/Johannes_Kepler"),
   Person("Galileo Galilei", 1564, 1642, "https://upload.wikimedia.org/wikipedia/commons/d/d4/Justus_Sustermans_-_Portrait_of_Galileo_Galilei%2C_1636.jpg", "https://en.wikipedia.org/wiki/Galileo_Galilei"),
-  Person("Isaac Newton", 1564, 1642, "https://upload.wikimedia.org/wikipedia/commons/8/83/Sir_Isaac_Newton_%281643-1727%29.jpg", "https://en.wikipedia.org/wiki/Isaac_Newton")
+  Person("Isaac Newton", 1643, 1727, "https://upload.wikimedia.org/wikipedia/commons/8/83/Sir_Isaac_Newton_%281643-1727%29.jpg", "https://en.wikipedia.org/wiki/Isaac_Newton"),
+  Person("Konstantin Tsiolkovsky", 1857, 1935, "https://upload.wikimedia.org/wikipedia/commons/9/92/Konstantin_Tsiolkovsky_1934.jpg", "https://en.wikipedia.org/wiki/Konstantin_Tsiolkovsky"),
+  Person("Robert H. Goddard", 1882, 1945, "https://upload.wikimedia.org/wikipedia/commons/3/3f/Dr._Robert_H._Goddard_-_GPN-2002-000131.jpg", "https://en.wikipedia.org/wiki/Robert_H._Goddard"),
+  Person("Hermann Oberth", 1894, 1989, "https://upload.wikimedia.org/wikipedia/commons/2/2b/Hermann_Oberth_1950s.jpg", "https://en.wikipedia.org/wiki/Hermann_Oberth"),
+  Person("Wernher von Braun", 1912, 1977, "https://upload.wikimedia.org/wikipedia/commons/5/56/Wernher_von_Braun_1960.jpg", "https://en.wikipedia.org/wiki/Wernher_von_Braun"),
+  Person("Sergei Korolev", 1907, 1966, "https://upload.wikimedia.org/wikipedia/commons/a/ab/%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B9_%D0%9F%D0%B0%D0%B2%D0%BB%D0%BE%D0%B2%D0%B8%D1%87_%D0%9A%D0%BE%D1%80%D0%BE%D0%BB%D1%91%D0%B2_%28%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82%29.jpg", "https://en.wikipedia.org/wiki/Sergei_Korolev")
 )
 
 const val pioneersPath = "/pioneers"
